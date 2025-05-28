552227882866e5afbd79f87613bbbaf42e676f35
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-05-28 12:16:10 -0500

    Create base Spring application

---
+++ src/main/kotlin/ronpotter99/astronomy/AstronomyApplication.kt
@@ -0,0 +1,11 @@
+package ronpotter99.astronomy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AstronomyApplication
+
+fun main(args: Array<String>) {
+	runApplication<AstronomyApplication>(*args)
+}
---
+++ src/test/kotlin/ronpotter99/astronomy/AstronomyApplicationTests.kt
@@ -0,0 +1,13 @@
+package ronpotter99.astronomy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AstronomyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

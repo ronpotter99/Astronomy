9e215f20e90e0d1829094f76b4e393dac5d0be85
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-06-09 14:56:17 -0500

    Start implementing kotlin operator methods on UBigDecimal class

--- src/main/kotlin/ronpotter99/astronomy/DTO/UBigDecimal.kt
+++ src/main/kotlin/ronpotter99/astronomy/DTO/UBigDecimal.kt
@@ -1,9 +1,48 @@
 package ronpotter99.astronomy.DTO
 
 import java.math.BigDecimal
-import org.hibernate.usertype.UserType
 
 data class UBigDecimal(
     var number: BigDecimal,
     var uncertainty: BigDecimal? = null
-)
+) {
+    operator fun unaryPlus(): UBigDecimal {
+        return UBigDecimal(number, uncertainty)
+    }
+
+    operator fun unaryMinus(): UBigDecimal {
+        return UBigDecimal(-number, uncertainty)
+    }
+    
+    operator fun inc(): UBigDecimal {
+        return (UBigDecimal(number, uncertainty) + UBigDecimal(BigDecimal("1")))
+    }
+    
+    operator fun dec(): UBigDecimal {
+        return (UBigDecimal(number, uncertainty) - UBigDecimal(BigDecimal("1")))
+    }
+    
+    operator fun plus(toAdd: UBigDecimal): UBigDecimal {
+        // TODO
+        return UBigDecimal(number, uncertainty)
+    }
+    
+    operator fun minus(toSubtract: UBigDecimal): UBigDecimal {
+        // TODO
+        return UBigDecimal(number, uncertainty)
+    }
+    
+    operator fun times(toMultiply: UBigDecimal): UBigDecimal {
+        // TODO
+        return UBigDecimal(number, uncertainty)
+    }
+    
+    operator fun div(toDivide: UBigDecimal): UBigDecimal {
+        // TODO
+        return UBigDecimal(number, uncertainty)
+    }
+    
+    operator fun rem(divisor: UBigDecimal): UBigDecimal {
+        return UBigDecimal(number % divisor.number, null)
+    }
+}

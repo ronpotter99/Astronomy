6d245d1eba7cf514e5b72192ee33f577b7c5306f
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-01 16:47:05 -0500

    Added equation to calculate Kepler's Third Law using standardized units instead of metric

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/KeplersThirdLawStandardized.kt
@@ -0,0 +1,48 @@
+package ronpotter99.astronomy.service.equation
+
+import ch.obermuhlner.math.big.DefaultBigDecimalMath as BDMath
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.math.BigDecimal
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+import ronpotter99.astronomy.utils.EquationConstants as Constants
+
+@Component
+class KeplersThirdLawStandardized : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "KeplersThirdLawStandardized"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "P" to "orbital period (yr)",
+                "M" to "mass of object being orbited (M_sun)",
+                "a" to "orbital semi-major axis (AU)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("P")) {
+                    (variables.get("a")!!.pow(BigDecimal("3")) / variables.get("M")!!).sqrt()
+                } else if (!variables.containsKey("M")) {
+                    (variables.get("a")!!.pow(BigDecimal("3")) / variables.get("P")!!.pow(BigDecimal("2")))
+                } else if (!variables.containsKey("a")) {
+                    (variables.get("P")!!.pow(BigDecimal("2")) * variables.get("M")!!).cbrt()
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

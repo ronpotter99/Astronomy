890f0c6957675a9fd0ece70ec6638e518113dadd
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-02 15:24:34 -0500

    Add equation to calculate angular separation of observed objects.

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/AngularSeparation.kt
@@ -0,0 +1,45 @@
+package ronpotter99.astronomy.service.equation
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+
+@Component
+class AngularSeparation : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "AngularSeparation"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "alpha" to "angular separation (rad)",
+                "a" to "distance between objects (m)",
+                "d" to "distance from viewer to objects (m)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("alpha")) {
+                    (variables.get("a")!! / variables.get("d")!!)
+                } else if (!variables.containsKey("a")) {
+                    (variables.get("alpha")!! * variables.get("d")!!)
+                } else if (!variables.containsKey("d")) {
+                    (variables.get("a")!! / variables.get("alpha")!!)
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

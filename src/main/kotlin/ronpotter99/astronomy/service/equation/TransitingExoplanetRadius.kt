c248db9b8e12f38654a456307fcdeaf7a7e4bbf1
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-02 15:26:03 -0500

    Add equation to calculate the radius of an exoplanet transiting in front of its star.

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/TransitingExoplanetRadius.kt
@@ -0,0 +1,55 @@
+package ronpotter99.astronomy.service.equation
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.math.BigDecimal
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+
+@Component
+class TransitingExoplanetRadius : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "TransitingExoplanetRadius"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "R_P" to "radius of the planet (m)",
+                "R_star" to "radius of the star (m)",
+                "Delta F" to "change in flux during the planet's transit (W m^-2)",
+                "F" to "baseline flux of the star not during transit (W m^-2)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("R_P")) {
+                    (variables.get("R_star")!! *
+                            (variables.get("Delta F")!! / variables.get("F")!!).sqrt())
+                } else if (!variables.containsKey("R_star")) {
+                    (variables.get("R_P")!! /
+                            (variables.get("Delta F")!! / variables.get("F")!!).sqrt())
+                } else if (!variables.containsKey("Delta F")) {
+                    ((variables.get("R_P")!! / variables.get("R_star")!!).pow(BigDecimal("2")) *
+                            variables.get("F")!!)
+                } else if (!variables.containsKey("F")) {
+                    (variables.get("Delta F")!! /
+                            (variables.get("R_P")!! / variables.get("R_star")!!).pow(
+                                    BigDecimal("2")
+                            ))
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

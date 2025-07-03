fd647626e0348a4d77e1e9a90e05bc2f21bb5d3d
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-03 12:49:19 -0500

    Add equation to calculate the mass of elliptical binaries

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/massAndGravitation/EllipticalBinaryMass.kt
@@ -0,0 +1,51 @@
+package ronpotter99.astronomy.service.equation.massAndGravitation
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+import ronpotter99.astronomy.service.equation.IEquation
+
+@Component
+class EllipticalBinaryMass : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "EllipticalBinaryMass"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "m_1" to "mass of star 1 (kg/M_sun)",
+                "m_2" to "mass of star 2 (kg/M_sun)",
+                "alpha_1" to "semi-major angular axis of star 1 (rad)",
+                "alpha_2" to "semi-major angular axis of star 2 (rad)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("m_1")) {
+                    ((variables.get("m_2")!! * variables.get("alpha_2")!!) /
+                            variables.get("alpha_1")!!)
+                } else if (!variables.containsKey("m_2")) {
+                    ((variables.get("m_1")!! * variables.get("alpha_1")!!) /
+                            variables.get("alpha_2")!!)
+                } else if (!variables.containsKey("alpha_1")) {
+                    ((variables.get("m_2")!! * variables.get("alpha_2")!!) / variables.get("m_1")!!)
+                } else if (!variables.containsKey("alpha_2")) {
+                    ((variables.get("m_1")!! * variables.get("alpha_1")!!) / variables.get("m_2")!!)
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

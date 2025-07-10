6b8f3b8b732c7f94dac92ece7a8d9144e179dda2
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-10 17:03:52 -0500

    Add equation to calculate the doppler effect based on light wavelength

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/light/DopplerEffect.kt
@@ -0,0 +1,50 @@
+package ronpotter99.astronomy.service.equation.massAndGravitation
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+import ronpotter99.astronomy.service.equation.IEquation
+import ronpotter99.astronomy.utils.EquationConstants as Constants
+
+@Component
+class DopplerEffect : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "DopplerEffect"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "lambda" to "original wavelength (m)",
+                "Delta lambda" to "change in wavelength (m)",
+                "v_r" to "radial velocity of source relative to observer (m/s)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("lambda")) {
+                    ((variables.get("Delta lambda")!! * Constants.SPEED_OF_LIGHT) /
+                            variables.get("v_r")!!)
+                } else if (!variables.containsKey("Delta lambda")) {
+                    ((variables.get("lambda")!! * variables.get("v_r")!!) /
+                            Constants.SPEED_OF_LIGHT)
+                } else if (!variables.containsKey("v_r")) {
+                    ((variables.get("Delta lambda")!! * Constants.SPEED_OF_LIGHT) /
+                            variables.get("lambda")!!)
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

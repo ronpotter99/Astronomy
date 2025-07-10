a00c556982ca4dbd7846a95ed703653096951fcd
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-07-10 17:04:36 -0500

    Add equation to convert from flux per unit frequency to flux per unit wavelength

---
+++ src/main/kotlin/ronpotter99/astronomy/service/equation/light/FluxDensityConversion.kt
@@ -0,0 +1,52 @@
+package ronpotter99.astronomy.service.equation.massAndGravitation
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.math.BigDecimal
+import org.springframework.stereotype.Component
+import ronpotter99.astronomy.DTO.ScientificNumber
+import ronpotter99.astronomy.service.equation.IEquation
+import ronpotter99.astronomy.utils.EquationConstants as Constants
+
+@Component
+class FluxDensityConversion : IEquation {
+
+    companion object {
+        const val EQUATION_REFERENCE: String = "FluxDensityConversion"
+    }
+
+    private val logger = KotlinLogging.logger {}
+
+    override fun getEquationReference(): String {
+        return EQUATION_REFERENCE
+    }
+
+    override fun getVariableList(): Map<String, String> {
+        return mapOf(
+                "F_v" to "Flux per unit of frequency (W m^-2 Hz^-1)",
+                "F_lambda" to "Flux per unit of wavelength (W m^-2 nm^-1)",
+                "lambda" to "wavelength (nm)"
+        )
+    }
+
+    override fun calculate(variables: Map<String, ScientificNumber>): ScientificNumber? {
+        validateInputVariables(variables)
+
+        val toReturn: ScientificNumber? =
+                if (!variables.containsKey("F_v")) {
+                    (variables.get("F_lambda")!! *
+                            (variables.get("lambda")!!.pow(BigDecimal("2")) /
+                                    Constants.SPEED_OF_LIGHT))
+                } else if (!variables.containsKey("F_lambda")) {
+                    ((variables.get("F_v")!! * Constants.SPEED_OF_LIGHT) /
+                            variables.get("lambda")!!.pow(BigDecimal("2")))
+                } else if (!variables.containsKey("lambda")) {
+                    ((variables.get("F_v")!! * Constants.SPEED_OF_LIGHT) /
+                            variables.get("F_lambda")!!)
+                } else {
+                    logger.warn { "$EQUATION_REFERENCE: Unknown variable to calculate." }
+                    null
+                }
+
+        return toReturn
+    }
+}

bd79ca71a3fd721f5745d4a96e66b400a6c15149
Thomas Stackhouse <thomasstackhouse99@gmail.com>
2025-08-26 13:21:32 -0500

    Fix small gradle issues and enable test dynamic agent loading for mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,68 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.0"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("com.adarshr.test-logger") version "4.0.0"
-	kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.0"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.adarshr.test-logger") version "4.0.0"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "ronpotter99"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("com.h2database:h2")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
     implementation("ch.obermuhlner:big-math:2.3.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-java-parameters")
-	}
-	jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-java-parameters")
+    }
+    jvmToolchain(21)
 }
 
 allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 configure<com.adarshr.gradle.testlogger.TestLoggerExtension> {
-	theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
+    theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA_PARALLEL
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	maxParallelForks = Runtime.getRuntime().availableProcessors().div(2) ?: 1
-	testlogger {
-		showSimpleNames = true
-	}
+    useJUnitPlatform()
+    maxParallelForks = Runtime.getRuntime().availableProcessors().div(2)
+    testlogger {
+        showSimpleNames = true
+    }
+}
+
+tasks {
+    test {
+        jvmArgs("-XX:+EnableDynamicAgentLoading")
+    }
 }
